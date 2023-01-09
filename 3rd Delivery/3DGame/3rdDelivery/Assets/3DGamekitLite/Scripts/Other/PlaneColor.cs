f326774d48d4bd502860cf0132618bbe68e343ce
Turmo11 <guillem.turmo.00@gmail.com>
2023-01-09 13:08:47 +0100

    Scene added
    He afegit una escena on podem muntar un nivell ràpid

---
+++ 3rd Delivery/3DGame/3rdDelivery/Assets/3DGamekitLite/Scripts/Other/PlaneColor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneColor : MonoBehaviour
+{
+    private MeshRenderer p_mesh_r;
+    private Material p_mat;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        p_mesh_r = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
